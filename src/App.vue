f1b56e57c8d1818ac4f2655fe3507de8906e4cc9
Thomas Trevino <ttrevino@stu.southtexascollege.edu>
2025-02-09 12:37:39 -0600

    Basic layout of the timer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
-
+import {ref} from 'vue'
 
 </script>
 
 
 <template>
+  <div class="container">
+    <h1>Countdown Timer</h1>
+    <input type="number" placeholder="Enter seconds" min="1" max="60" v-model.number="timeInput"/>
 
+    <button>Start</button>
+    <button>Stop</button>
+
+    <h2 class="timerDisplay">00:00</h2>
+
+  </div>
 
 
 </template>
