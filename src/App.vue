a6b98af5ec8dfc6d233f4456dbd557605d987bcb
Thomas Trevino <ttrevino@stu.southtexascollege.edu>
2025-02-09 13:54:43 -0600

    Added the rest of the code and a base property in vite.config.js

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,39 @@
 <script setup>
-import {ref} from 'vue'
+import {ref, computed} from 'vue'
 
+const timeInput = ref(null)
+const timeLeft = ref(0)
+let countdown = null
+
+const formattedTime = computed(() => {
+  return `00:${timeLeft.value < 10 ? '0' : ''}${timeLeft.value}`
+})
+
+const startCountdown = () => {
+  stopCountdown()
+
+  if (isNaN(timeInput.value) || timeInput.value < 1 || timeInput.value > 60) {
+    alert("Please enter a number between 1 and 60")
+    return
+  }
+
+  timeLeft.value = timeInput.value
+
+  countdown = setInterval(() => {
+    if (timeLeft.value <= 0) {
+      stopCountdown();
+      alert("Time's up!");
+    } else {
+      timeLeft.value--;
+    }
+  }, 1000)
+}
+
+const stopCountdown = () => {
+  clearInterval(countdown)
+  countdown = null
+  timeLeft.value = 0
+}
 </script>
 
 
@@ -9,12 +42,10 @@ import {ref} from 'vue'
     <h1>Countdown Timer</h1>
     <input type="number" placeholder="Enter seconds" min="1" max="60" v-model.number="timeInput"/>
 
-    <button>Start</button>
-    <button>Stop</button>
+    <button @click="startCountdown" id="startBtn">Start</button>
+    <button @click="stopCountdown" id="stopBtn">Stop</button>
 
-    <h2 class="timerDisplay">00:00</h2>
+    <h2 id="timerDisplay">{{ formattedTime }}</h2>
 
   </div>
-
-
 </template>
